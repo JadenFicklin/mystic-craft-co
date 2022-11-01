fd8d42d4b2228496a4ce5158925843a61bf9c9a2
JadenFicklin <jadendficklin@gmail.com>
2022-11-01 12:19:12 -0600

    minor fixes to nav bar and updated shopping page

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "../styles/Shop.css";
 
 function Shop() {
-  return <div>Shop</div>;
+  return (
+    <>
+      <div className="shop-outer">
+        <div className="shop-inner">Shopping page</div>
+        <div className="shop-tab">
+          <div className="shop-tab-rounded-bit"></div>
+        </div>
+        <div className="shopping-page-outer"></div>
+      </div>
+    </>
+  );
 }
 
 export default Shop;
