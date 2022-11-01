1e27c374bf8eedf6224f884b7a3437ec240fe91b
JadenFicklin <jadendficklin@gmail.com>
2022-10-31 22:01:17 -0600

    updates to nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Nav.css";
 
 function Nav() {
+  const [dropdown, setDropdown] = useState(false);
+
+  const navDropDown = () => {
+    setDropdown(!dropdown);
+  };
+
   return (
     <>
-      <nav>
-        <Link to="/" className="nav-link">
-          Home
-        </Link>
-        <Link to="/shop" className="nav-link">
-          shop
-        </Link>
-        <Link to="/forum" className="nav-link">
-          forum
-        </Link>
-        <Link to="/contact" className="nav-link">
-          contact
-        </Link>
-      </nav>
+      <div className="nav-bars">
+        <div className="inner-nav-bars" onClick={() => navDropDown()}>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+      </div>
+      {dropdown && (
+        <nav>
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+          <Link to="/shop" className="nav-link">
+            shop
+          </Link>
+          <Link to="/forum" className="nav-link">
+            forum
+          </Link>
+          <Link to="/contact" className="nav-link">
+            contact
+          </Link>
+        </nav>
+      )}
     </>
   );
 }
