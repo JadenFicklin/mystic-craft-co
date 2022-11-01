1d07c25978aae2a5af62707d6dab8afb20a9efaa
JadenFicklin <jadendficklin@gmail.com>
2022-11-01 10:28:34 -0600

    updates to nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Nav.css";
+import { AiFillHome, AiOutlineShop } from "react-icons/ai";
+import { MdForum } from "react-icons/md";
+import { GrContact } from "react-icons/gr";
 
 function Nav() {
   const [dropdown, setDropdown] = useState(false);
@@ -11,29 +14,45 @@ function Nav() {
 
   return (
     <>
-      <div className="nav-bars">
-        <div className="inner-nav-bars" onClick={() => navDropDown()}>
+      <div className="nav-bars" onClick={() => navDropDown()}>
+        <div className="inner-nav-bars">
           <div className="line"></div>
           <div className="line"></div>
           <div className="line"></div>
         </div>
       </div>
-      {dropdown && (
-        <nav>
-          <Link to="/" className="nav-link">
-            Home
-          </Link>
-          <Link to="/shop" className="nav-link">
-            shop
-          </Link>
-          <Link to="/forum" className="nav-link">
-            forum
-          </Link>
-          <Link to="/contact" className="nav-link">
-            contact
-          </Link>
-        </nav>
-      )}
+      <div className={dropdown ? "nav-outer" : "nav-outer opacity"}>
+        <div className={dropdown ? "nav false" : "nav"}>
+          <div className="nav-inner">
+            <div className="nav-section-one">
+              <Link to="/" className="nav-link">
+                <div className="link-left">
+                  <AiFillHome className="icon" />
+                </div>
+                <div className="link-right">Home</div>
+              </Link>
+              <Link to="/shop" className="nav-link">
+                <div className="link-left">
+                  <AiOutlineShop className="icon" />
+                </div>
+                <div className="link-right">Shop</div>
+              </Link>
+              <Link to="/forum" className="nav-link">
+                <div className="link-left">
+                  <MdForum className="icon" />
+                </div>
+                <div className="link-right">Forum</div>
+              </Link>
+              <Link to="/contact" className="nav-link">
+                <div className="link-left">
+                  <GrContact className="icon" />
+                </div>
+                <div className="link-right">Contact</div>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
